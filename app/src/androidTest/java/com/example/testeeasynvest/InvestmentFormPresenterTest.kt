23c21a8af47935edb4008cfc62ff3651364a4e3d
caio.e.nascimento <caio.e.nascimento@accenture.com>
2019-08-31 01:46:28 -0300

    Criada classe de testes

---
+++ app/src/androidTest/java/com/example/testeeasynvest/InvestmentFormPresenterTest.kt
@@ -0,0 +1,13 @@
+package com.example.testeeasynvest
+
+import com.example.testeeasynvest.models.InvestmentRequest
+import com.example.testeeasynvest.models.InvestmentResponse
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.example.testeeasynvest.ui.investment_form.InvestmentFormContract
+import com.example.testeeasynvest.ui.investment_form.InvestmentFormPresenter
+import org.junit.Test
+
+class InvestmentFormPresenterTest {
+
+}
--- app/src/test/java/com/example/testeeasynvest/ExampleUnitTest.kt
+++ app/src/test/java/com/example/testeeasynvest/ExampleUnitTest.kt
@@ -1,8 +1,19 @@
 package com.example.testeeasynvest
 
+import com.example.testeeasynvest.di.component.DaggerActivityComponent
+import com.example.testeeasynvest.di.module.ActivityModule
+import com.example.testeeasynvest.models.InvestmentRequest
+import com.example.testeeasynvest.models.InvestmentResponse
+import com.example.testeeasynvest.ui.investment_form.InvestmentFormActivity
+import com.example.testeeasynvest.ui.investment_form.InvestmentFormContract
+import com.example.testeeasynvest.ui.investment_form.InvestmentFormPresenter
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.mockito.MockitoAnnotations
+import javax.inject.Inject
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +21,37 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    lateinit var presenter: InvestmentFormContract.Presenter
+
+    lateinit var view: InvestmentFormContract.View
+
+    private lateinit var request: InvestmentRequest
+    private lateinit var response: InvestmentResponse
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Before
+    fun setup() {
+
+        presenter = InvestmentFormPresenter()
+
+        MockitoAnnotations.initMocks(this)
+
+        view = mock()
+        presenter.attach(view)
+    }
+
+    @Test
+    fun call_requestInvestmentSuccess() {
+
+        request = InvestmentRequest(1000.0, "CDI", 100, false, "2020-02-11")
+
+        presenter.presentSendDataInvestment(request)
+
+        verify(view).viewRequestInvestment(response)
     }
 }
